aa66816cf399924e5da5b07c63fceb2702c80fbf
Patrick Boynton <youremail@yourdomain.com>
2024-09-12 15:37:56 -0400

    Added a Mapping function.

--- Dtos/VideoNavigationDto.cs
+++ Dtos/VideoNavigationDto.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using R3vids.Data;
 using R3vids.Models;
 
 namespace R3vids.Dtos;
@@ -5,6 +7,27 @@ namespace R3vids.Dtos;
 public class VideoNavigationDto
 {
     public Guid Id { get; set; }
-    public Video? CurrentVideo { get; set; }
-    public Video? PreviousVideo { get; set; }
+    public VideoDto? CurrentVideo { get; set; }
+    public VideoDto? PreviousVideo { get; set; }
+
+    public static VideoNavigationDto MapToDto(VideoNavigation videoNavigation, VideoDbContext context)
+    {
+        //     if (!videoNavigation.CurrentVideo.HasValue) throw new Exception("Current video is null.");
+        //
+        var id = videoNavigation?.CurrentVideo.Value;
+
+        if (id == null) throw new Exception("Current video id is null.");
+        ;
+        var video = context.Videos.Include(v => v.VideoStatus).FirstOrDefault(v => v.Id == id);
+
+
+        if (video == null) throw new Exception("Video not found.");
+
+        return new VideoNavigationDto
+        {
+            Id = videoNavigation.Id,
+            CurrentVideo = video != null ? VideoDto.MapToDto(video) : null,
+            PreviousVideo = null
+        };
+    }
 }
