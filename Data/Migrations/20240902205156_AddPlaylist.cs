cf6c90f4bea48e1e11d4c70ee7171dc83846743e
Patrick Boynton <youremail@yourdomain.com>
2024-09-12 15:33:06 -0400

    Added the migration for the new playlist feature.

---
+++ Data/Migrations/20240902205156_AddPlaylist.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace R3vids.Data.Migrations
+{
+    /// <inheritdoc />              
+    public partial class AddPlaylist : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PlaylistId",
+                table: "Videos",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Playlists",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Playlists", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Videos_PlaylistId",
+                table: "Videos",
+                column: "PlaylistId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Videos_Playlists_PlaylistId",
+                table: "Videos",
+                column: "PlaylistId",
+                principalTable: "Playlists",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Videos_Playlists_PlaylistId",
+                table: "Videos");
+
+            migrationBuilder.DropTable(
+                name: "Playlists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Videos_PlaylistId",
+                table: "Videos");
+
+            migrationBuilder.DropColumn(
+                name: "PlaylistId",
+                table: "Videos");
+        }
+    }
+}
