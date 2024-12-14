5e43935972599bd2e606ea769bd4bf8ea58786d9
Patrick Boynton <youremail@yourdomain.com>
2024-12-14 15:24:53 -0500

    Added a selection count column to the video status table.

--- Models/VideoStatus.cs
+++ Models/VideoStatus.cs
@@ -6,8 +6,9 @@ public class VideoStatus
     public bool Played { get; set; } = false;
     public decimal CurrentPlayTime { get; set; } = 0;
     public int PlayCount { get; set; } = 0;
+    public int SelectionCount { get; set; } = 0;
     public bool IsWatchLater { get; set; } = false;
     public DateTime? LastPlayed { get; set; }
     public Video Video { get; set; } = null!;
     public Guid VideoId { get; set; }
-}       
+}
